9f66e00bd388ee5d98923f83c01d420d7e611368
omarmbareche <99615404+rektow@users.noreply.github.com>
2024-04-02 13:50:07 -0400

    Update INSERT INTO patients (nom, prenom, date_.sql

--- INSERT INTO patients (nom, prenom, date_.sql
+++ INSERT INTO patients (nom, prenom, date_.sql
@@ -2,4 +2,4 @@ INSERT INTO patients (nom, prenom, date_de_naissance, numero_secu) VALUES
 ('Dupont', 'Jean', '1980-02-15', '1234567890123'),
 ('Martin', 'Marie', '1992-07-24', '9876543210987');
 
--- Ajoutez des instructions INSERT pour chaque table
+
